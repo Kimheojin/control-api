586edd67e998f9535d1d5feeb6623b61ced08cb4
Jin <hurjin1109@naver.com>
2026-06-23 18:59:43 +0900

    INIT

---
+++ src/test/java/heojin/control_api/ControlApiApplicationTests.java
@@ -0,0 +1,13 @@
+package heojin.control_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
